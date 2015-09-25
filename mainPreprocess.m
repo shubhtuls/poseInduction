5a103a3be50da5044ac74554203373fbe2c73a01
Shubham Tulsiani <shubhtuls@gmail.com>
2015-09-24 21:33:34 -0700

    uncomment preprocessing

--- mainPreprocess.m
+++ mainPreprocess.m
@@ -4,28 +4,28 @@ function [] = mainPreprocess()
 
 globals;
 %% rigid classes
-%for c = params.classInds
-%    class = pascalIndexClass(c, 'pascal');
-%    readData(class);
-%end
+for c = params.classInds
+    class = pascalIndexClass(c, 'pascal');
+    readData(class);
+end
 
 %% articulated classes
-%for c = params.articulatedInds
-%    class = pascalIndexClass(c, 'pascal');
-%    pascalData = readDataPascal(class,'2012');
-%    fname = fullfile(rotationPascalDataDir,class);
-%    rotationData = pascalData;
-%    save(fname,'rotationData');
-%end
+for c = params.articulatedInds
+    class = pascalIndexClass(c, 'pascal');
+    pascalData = readDataPascal(class,'2012');
+    fname = fullfile(rotationPascalDataDir,class);
+    rotationData = pascalData;
+    save(fname,'rotationData');
+end
 
-%for c = params.articulatedInds
-%    class = pascalIndexClass(c, 'pascal');
-%    augmentArticulatedPose(class);
-%end
+for c = params.articulatedInds
+    class = pascalIndexClass(c, 'pascal');
+    augmentArticulatedPose(class);
+end
 
 %% generate files for cnn training
 vpsPascalDataCollect()
 vpsImagenetDataCollect()
 rcnnBinnedJointTrainValTestCreate('');
- 
+
 end
